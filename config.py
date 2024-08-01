040b4e895002f4c8be42978c3ebd29f4532f2acd
Blackhat9009009 <biharkumar9009@gmail.com>
2024-08-01 21:35:21 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY @jack_sparrow_offical
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7405174587:AAEATnzR9guGENZALrNAfkRf52F-BALzZ48")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("venomXcrazy")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("6101783778")
+
+
+
+
+
+
+
+  
